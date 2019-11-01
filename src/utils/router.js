a1176e23f90eaf62bd04dade7f87e6c95940ee93
Daniel romero <daniel@bidibonus.com>
2019-11-01 02:53:20 +0100

    router

--- src/utils/router.js
+++ src/utils/router.js
@@ -7,10 +7,6 @@ export const initRouter = (element) => {
   router.setRoutes([
     ...router.getRoutes(),
     {
-      path: '/javascript',
-      component: 'home-template-view'
-    },
-    {
       path: '/',
       component: 'home-template-view'
     },
